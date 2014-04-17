e83f7abdc8e9ef1b1e56cd4bc48a2421b903fbcc
Stefan Schlott <stefan@ploing.de>
2014-04-17 12:34:12 +0200

    Fix filter

--- src/main/java/de/cccs/filterretweeter/filters/FilterBlockedUsers.java
+++ src/main/java/de/cccs/filterretweeter/filters/FilterBlockedUsers.java
@@ -14,39 +14,39 @@ import de.cccs.filterretweeter.Filter;
 
 
 public class FilterBlockedUsers implements Filter {
-	final static Logger logger = LoggerFactory.getLogger(FilterBlockedUsers.class);
-	public final int INTERVAL = 15; // Update every 15 mins
-	final Twitter twitter;
-	List<Long> blockedIDs = new ArrayList<Long>();
-	Date lastUpdate = null;
-	
-	public FilterBlockedUsers(Twitter twitter) {
-		this.twitter = twitter;
-	}
-	
-	void updateBlockList() {
-		Date now = new Date();
-		if (lastUpdate==null || (lastUpdate.getTime()+60*INTERVAL)<now.getTime()) {
-			try {
-				long ids[] = twitter.getBlocksIDs().getIDs();
-				blockedIDs = new ArrayList<Long>(ids.length);
-				for (long id: ids) {
-					blockedIDs.add(id);
-				}
-			} catch (TwitterException e) {
-				logger.warn("Unable to get block list", e);
-			}
-			lastUpdate = now;
-		}
-	}
+    final static Logger logger = LoggerFactory.getLogger(FilterBlockedUsers.class);
+    public final int INTERVAL = 15; // Update every 15 mins
+    final Twitter twitter;
+    List<Long> blockedIDs = new ArrayList<Long>();
+    Date lastUpdate = null;
 
-	@Override
-	public boolean filterTweet(Status status) {
-		updateBlockList();
-		return blockedIDs.contains(status.getId());
-	}
+    public FilterBlockedUsers(Twitter twitter) {
+        this.twitter = twitter;
+    }
 
-	@Override
-	public void notify(Status status) {
-	}
+    void updateBlockList() {
+        Date now = new Date();
+        if (lastUpdate == null || (lastUpdate.getTime() + 60 * INTERVAL) < now.getTime()) {
+            try {
+                long ids[] = twitter.getBlocksIDs().getIDs();
+                blockedIDs = new ArrayList<Long>(ids.length);
+                for (long id : ids) {
+                    blockedIDs.add(id);
+                }
+            } catch (TwitterException e) {
+                logger.warn("Unable to get block list", e);
+            }
+            lastUpdate = now;
+        }
+    }
+
+    @Override
+    public boolean filterTweet(Status status) {
+        updateBlockList();
+        return blockedIDs.contains(status.getUser().getId());
+    }
+
+    @Override
+    public void notify(Status status) {
+    }
 }
